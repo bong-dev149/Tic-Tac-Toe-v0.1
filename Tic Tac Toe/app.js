77ad0f2195b59dd4307121a9d1a24b482612464c
tya149 <64201418+tya149@users.noreply.github.com>
2020-08-01 10:35:02 +0530

    Update app.js

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -69,6 +69,17 @@ function compTurn(x) {
         var tmp = 0;
         console.log('tmp  = '+tmp);
 
+        if (mainMatrix[2][1]==='O' && (mainMatrix[1][0]||mainMatrix[1][2])) {
+            if(mainMatrix[1][1]===0 && tmp === 0) {
+                mainMatrix[1][1]='X';
+                showX(5);
+                console.log('here');
+                prev=5;
+                tmp=1;
+                console.log(tmp);
+            }
+        }
+
         if (mainMatrix[2][1]==='X' && mainMatrix[1][1]==='X' && tmp ===0) {
             if(mainMatrix[0][1]===0 && tmp === 0) {
                 mainMatrix[0][1]='X';
